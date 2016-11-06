30a577e91b7573e0b92a467e7444da08b27c5b1a
ZINGDING <tjgmltn07@naver.com>
2016-11-06 19:36:50 +0900

    . (#1)
    * Upload ZINGDING-EULA
    
    Upload ZINGDING-EULA.
    
    * Clean code

--- src/main/java/zingding/ValidEvent/ValidEvent.java
+++ src/main/java/zingding/ValidEvent/ValidEvent.java
@@ -1,6 +1,7 @@
 package zingding.ValidEvent;
 
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
@@ -8,18 +9,16 @@ import cn.nukkit.event.player.PlayerInvalidMoveEvent;
 
 public class ValidEvent extends PluginBase implements Listener {
 	@Override
-	public void onEnable(){
-	this.getLogger().info(getName());
-	this.getLogger().notice(TextFormat.RED + "본 플러그인은 ZINGDING-EULA가 적용되어 있습니다.");
-	this.getLogger().notice(TextFormat.AQUA + "플러그인이 활성화 되었습니다!");
+	public void onEnable() {
+		this.getLogger().info(getName());
+		this.getLogger().notice(TextFormat.RED + "본 플러그인은 ZINGDING-EULA가 적용되어 있습니다.");
+		this.getLogger().notice(TextFormat.AQUA + "플러그인이 활성화 되었습니다!");
 	}
-	
-	@EventHandler
-	public void onInvalidMove(PlayerInvalidMoveEvent event){
-	
-		if(event.getPlayer() != null){
-		event.setRevert(false);
 
-}
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onInvalidMove(PlayerInvalidMoveEvent event) {
+		if (event.getPlayer().isOnline()) {
+			event.setCancelled();
+		}
 	}
 }
